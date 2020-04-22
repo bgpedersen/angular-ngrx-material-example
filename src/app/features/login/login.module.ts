d808cd6ad66797909852700453ffa48714b6b3c0
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-04-22 08:54:12 +0200

    Add material

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,11 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginComponent } from './components/login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -15,11 +11,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
   imports: [
     CommonModule,
     LoginRoutingModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
+    SharedModule,
     ReactiveFormsModule,
   ],
 })
