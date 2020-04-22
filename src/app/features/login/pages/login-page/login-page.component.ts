ba15129b4a9aa679b007f09527ad520b20d6041c
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-04-22 07:15:55 +0200

    Run prettier

--- src/app/features/login/pages/login-page/login-page.component.ts
+++ src/app/features/login/pages/login-page/login-page.component.ts
@@ -4,13 +4,10 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
