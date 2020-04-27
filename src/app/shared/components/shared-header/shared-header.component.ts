21690eb627f87b5b8f530410816ce5dcdd25bbaa
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-04-27 17:54:17 +0200

    Add logout btn

--- src/app/shared/components/shared-header/shared-header.component.ts
+++ src/app/shared/components/shared-header/shared-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 import { ThemeService } from 'src/app/core/services/theme.service';
 
 @Component({
@@ -7,7 +9,20 @@ import { ThemeService } from 'src/app/core/services/theme.service';
   styleUrls: ['./shared-header.component.scss'],
 })
 export class SharedHeaderComponent implements OnInit {
-  constructor(private themeService: ThemeService) {}
+  constructor(
+    private themeService: ThemeService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.authService.setAuthenticated(false);
+    this.router.navigateByUrl('login');
+  }
+
+  get isAuthenticated() {
+    return this.authService.getAuthenticated();
+  }
 
   toggleTheme() {
     this.themeService.toggleTheme();
