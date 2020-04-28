6b8b6b246157c95bbdaa4078abdba755f32dfdf8
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-04-28 14:20:47 +0200

    Store init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppEffects } from './app.effects';
 import { LoginModule } from './features/login/login.module';
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +19,15 @@ import { LoginModule } from './features/login/login.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      },
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects]),
     LoginModule,
   ],
   providers: [],
