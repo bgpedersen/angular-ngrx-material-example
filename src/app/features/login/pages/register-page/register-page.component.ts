dcba92b2fbdcbb3cae98c3a34f87dd60d19096b7
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-04-30 15:59:22 +0200

    Text fix

--- src/app/features/login/pages/register-page/register-page.component.ts
+++ src/app/features/login/pages/register-page/register-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { User } from 'src/app/core/models/user.model';
-import { createUser } from '../../actions/register-page.actions';
+import * as RegisterPageActions from '../../actions/register-page.actions';
 
 @Component({
   selector: 'app-register-page',
@@ -9,27 +9,11 @@ import { createUser } from '../../actions/register-page.actions';
   styleUrls: ['./register-page.component.scss'],
 })
 export class RegisterPageComponent implements OnInit {
-  constructor(
-    // private router: Router,
-    // private userService: UsersService,
-    // private snackBar: MatSnackBar
-    private store: Store
-  ) {}
+  constructor(private store: Store) {}
 
   ngOnInit(): void {}
 
   onFormSubmit(user: User) {
-    this.store.dispatch(createUser(user));
-
-    // this.userService.post(user).subscribe(
-    //   (res) => {
-    //     this.snackBar.open('Success! User created');
-    //     this.router.navigateByUrl('');
-    //   },
-    //   (err) => {
-    //     this.snackBar.open(`Error: ${err}`);
-    //     console.error(err);
-    //   }
-    // );
+    this.store.dispatch(RegisterPageActions.createUser(user));
   }
 }
